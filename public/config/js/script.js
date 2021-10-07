fdd8d3e37174dc61c34e4be408edd8a81acaa7da
garycu <89985066+garycu@users.noreply.github.com>
2021-10-06 22:38:44 -0400

    First commit

---
+++ public/config/js/script.js
@@ -0,0 +1,83 @@
+// JavaScript Document
+
+$(document).ready(function() {
+	/* ScrollIt Start */
+	$.scrollIt({
+		upKey: 38, // key code to navigate to the next section
+		downKey: 40, // key code to navigate to the previous section
+		easing: 'linear', // the easing function for animation
+		scrollTime: 600, // how long (in ms) the animation takes
+		activeClass: 'active', // class given to the active nav element
+		onPageChange: null, // function(pageIndex) that is called when page is changed
+		topOffset: -109 // offste (in px) for fixed top navigation
+	});
+	/* ScrollIt End */
+
+	// Sticky Header
+	$(window).scroll(function () {
+		var scroll = $(window).scrollTop();
+
+		if (scroll >= 100) {
+			$(".header").addClass("header-stick");
+		} else {
+			$(".header").removeClass("header-stick");
+		}
+	});
+
+	$('.navbar-toggler .navbar-toggler-icon').click(function () {
+		$(this).toggleClass('open');
+		// $('.nav').toggleClass('open');
+	});
+	// var scroll = $(window).scrollTop();
+
+	// if (scroll >= 100) {
+	// 	$(".header").addClass("header-stick");
+	// } else {
+	// 	$(".header").removeClass("header-stick");
+	// }
+
+	/* CounterDown Timer Start */
+	$('.custom_timer').countdown({until: new Date(1633960800 * 1000), format: 'DHMS', padZeroes: true, compact: true});
+	/* CounterDown Timer End */
+
+	/* Skywalker Carousel */
+	$(".skywalker-carousel").slick({
+		dots: false,
+		arrows: false,
+		infinite: true,
+		autoplay: true,
+		speed: 10000,
+  		autoplaySpeed: 0,
+		slidesToShow: 4,
+		slidesToScroll: 1,
+		responsive: [
+		{
+			breakpoint: 1199,
+			settings: {
+				slidesToShow: 3,
+				slidesToScroll: 1,
+			}
+		},
+		{
+			breakpoint: 768,
+			settings: {
+				slidesToShow: 2,
+				slidesToScroll: 1
+			}
+		},
+		{
+			breakpoint: 576,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1
+			}
+		}
+			// You can unslick at a given breakpoint now by adding:
+			// settings: "unslick"
+			// instead of a settings object
+		]
+	  });
+	/* Skywalker Carousel */
+	// OFI Browser
+	objectFitImages();
+});
--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ export const StyledRoundButton = styled.button`
   padding: 10px;
   border-radius: 100%;
   border: none;
-  background-color: var(--primary);
+  background-color: white;
   padding: 10px;
   font-weight: bold;
   font-size: 15px;
@@ -76,7 +76,7 @@ export const StyledLogo = styled.img`
 
 export const StyledImg = styled.img`
   box-shadow: 0px 5px 11px 2px rgba(0, 0, 0, 0.7);
-  border: 4px dashed var(--secondary);
+  border: 4px solid white;
   background-color: var(--accent);
   border-radius: 100%;
   width: 200px;
@@ -198,10 +198,12 @@ function App() {
       <s.Container
         flex={1}
         ai={"center"}
-        style={{ padding: 24, backgroundColor: "var(--primary)" }}
+        style={{ padding: 50, backgroundColor: "var(--primary)" }}
         image={CONFIG.SHOW_BACKGROUND ? "/config/images/bg.png" : null}
       >
-        <StyledLogo alt={"logo"} src={"/config/images/logo.png"} />
+        <div class="section-title">
+				<h2>// MINT YOUR SKYWALKER //</h2>
+			</div>
         <s.SpacerSmall />
         <ResponsiveWrapper flex={1} style={{ padding: 24 }} test>
           <s.Container flex={1} jc={"center"} ai={"center"}>
@@ -216,7 +218,7 @@ function App() {
               backgroundColor: "var(--accent)",
               padding: 24,
               borderRadius: 24,
-              border: "4px dashed var(--secondary)",
+              border: "4px solid white",
               boxShadow: "0px 5px 11px 2px rgba(0,0,0,0.7)",
             }}
           >
@@ -370,20 +372,13 @@ function App() {
             <s.SpacerMedium />
           </s.Container>
           <s.SpacerLarge />
-          <s.Container flex={1} jc={"center"} ai={"center"}>
-            <StyledImg
-              alt={"example"}
-              src={"/config/images/example.gif"}
-              style={{ transform: "scaleX(-1)" }}
-            />
-          </s.Container>
         </ResponsiveWrapper>
         <s.SpacerMedium />
         <s.Container jc={"center"} ai={"center"} style={{ width: "70%" }}>
           <s.TextDescription
             style={{
               textAlign: "center",
-              color: "var(--primary-text)",
+              color: "white",
             }}
           >
             Please make sure you are connected to the right network (
@@ -394,7 +389,7 @@ function App() {
           <s.TextDescription
             style={{
               textAlign: "center",
-              color: "var(--primary-text)",
+              color: "white",
             }}
           >
             We have set the gas limit to {CONFIG.GAS_LIMIT} for the contract to
